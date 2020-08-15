2c753d588ca2c1711dafcddaa0859e51a20163e8
YaBoiJager <cragmacfield@gmail.com>
2020-08-15 19:03:22 +0700

    Add better jumping

---
+++ Bitbot/Assets/Scripts/JumpModifier.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class JumpModifier : MonoBehaviour
+{
+    [Header("Multiplers")]
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
+
+    private Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    void FixedUpdate()
+    {
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (rb.velocity.y > 0 && !Input.GetButton("Jump"))
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+    }
+}
