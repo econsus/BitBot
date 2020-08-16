81e779548798ebcc31dfce2b23b432d773782690
YaBoiJager <cragmacfield@gmail.com>
2020-08-16 13:49:06 +0700

    Use custom class

--- Bitbot/Assets/Scripts/CrosshairMovement.cs
+++ Bitbot/Assets/Scripts/CrosshairMovement.cs
@@ -2,21 +2,14 @@
 
 public class CrosshairMovement : MonoBehaviour
 {
-    [Header("Deadzone")]
-    public float z = 6f;
-
-    private Transform xhair;
     void Start()
     {
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Confined;
-        xhair = GetComponent<Transform>();
     }
     void FixedUpdate()
     {
-        Vector3 screenMPos = Input.mousePosition;
-        Vector3 worldMPos = Camera.main.ScreenToWorldPoint(new Vector3(screenMPos.x, screenMPos.y, z));
-        xhair.position = new Vector3(worldMPos.x, worldMPos.y, xhair.position.z);
-        Debug.Log("MPOS: " + xhair.position);
+        Vector3 worldMPos = MouseWorldPosition.getMouseWorldPos(0f);
+        this.transform.position = new Vector3(worldMPos.x, worldMPos.y, this.transform.position.z);
     }
 }
