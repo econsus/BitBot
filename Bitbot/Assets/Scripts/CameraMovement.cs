b2ccc8bc9968be54c573f8e9cf081cece7b01c1d
YaBoiJager <cragmacfield@gmail.com>
2020-08-16 13:47:22 +0700

    Update CameraMovement.cs

--- Bitbot/Assets/Scripts/CameraMovement.cs
+++ Bitbot/Assets/Scripts/CameraMovement.cs
@@ -7,33 +7,18 @@ public class CameraMovement : MonoBehaviour
     
     public float dampingTIme = 1f;
 
-    private float maxSpeed;
-
-    private PlayerMovement move;
     private CinemachineVirtualCamera vcam;
     void Start()
     {
-        move = FindObjectOfType<PlayerMovement>();
         vcam = GetComponent<CinemachineVirtualCamera>();
     }
-    void LateUpdate()
+    void Update()
     {
-        flipScreenX(move.facingRight);
+
     }
 
     private void flipScreenX(bool facingRight)
     {
-        float flipLeft = Mathf.SmoothDamp(0.88f, 0.18f,ref maxSpeed, dampingTIme);
-        float flipRight = Mathf.SmoothDamp(0.18f, 0.88f, ref maxSpeed, dampingTIme);
-
-        if (!move.facingRight)
-        {
-            vcam.GetCinemachineComponent<CinemachineFramingTransposer>().m_ScreenX = flipLeft;
-        }
-        else
-        {
-
-            vcam.GetCinemachineComponent<CinemachineFramingTransposer>().m_ScreenX = flipRight;
-        }
+        
     }
 }
