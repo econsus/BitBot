f277380a16250bb1c9a9b427eb871469e05e5a6c
YaBoiJager <cragmacfield@gmail.com>
2020-08-16 13:48:36 +0700

    Add custom class to get mouse world position

---
+++ Bitbot/Assets/Scripts/Class/MouseWorldPosition.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class MouseWorldPosition
+{
+    public static Vector3 getMouseWorldPos(float z)
+    {
+        Vector3 foo = calcWorldPos(Input.mousePosition, Camera.main);
+        foo.z = z;
+        return foo;
+    }
+
+    public static Vector3 calcWorldPos(Vector3 screenPos, Camera worldCam)
+    {
+        Vector3 worldPos = worldCam.ScreenToWorldPoint(screenPos);
+        return worldPos;
+    }
+}
+
