7c95fee28fa630bb173d35a7a6266fbf9a9db4bc
YaBoiJager <cragmacfield@gmail.com>
2020-08-19 21:37:07 +0700

    Add door enter symbol

---
+++ Bitbot/Assets/Scripts/DoorEnter.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class DoorEnter : MonoBehaviour
+{
+    private bool inFrontofDoor = false;
+    private GameObject symbol;
+    void Start()
+    {
+        symbol = GameObject.Find("Door Enter");
+        symbol.SetActive(false);
+    }
+    void Update()
+    {
+        showSymbol();
+
+        if(Input.GetKey(KeyCode.E) && inFrontofDoor)
+        {
+            Debug.Log("ENTERED DOOR");
+        }
+    }
+
+    private void showSymbol()
+    {
+        symbol.SetActive(inFrontofDoor);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player") && !collision.isTrigger)
+        {
+            inFrontofDoor = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && !collision.isTrigger)
+        {
+            inFrontofDoor = false;
+        }
+    }
+
+}
