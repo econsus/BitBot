db570733aceeffc4481af9fef9fec2b24b8ff67b
YaBoiJager <cragmacfield@gmail.com>
2020-08-26 19:20:41 +0700

    Start developing inventory system

---
+++ Bitbot/Assets/Scripts/Scriptable Objects/Items/WeaponObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum WeaponType
+{
+    Ranged,
+    Melee
+}
+
+public abstract class WeaponObject : ItemObject
+{
+    public WeaponType weaponType;
+}
+
