44c0c5b58367db6cff1a46b10a8485937b3b6581
YaBoiJager <cragmacfield@gmail.com>
2020-09-03 01:27:28 +0700

    Add underground level & camera switch between rooms

---
+++ Bitbot/Assets/Scripts/Behaviours/UI Scripts/CameraSwitch.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitch : MonoBehaviour
+{
+    private GameObject cam;
+    private PlayerMovement move;
+    private bool startTransition = false;
+    void Awake()
+    {
+        cam = transform.GetChild(0).gameObject;
+        move = GameObject.Find("Player").GetComponent<PlayerMovement>();
+    }
+
+    private void Update()
+    {
+        if(startTransition)
+        {
+            StartCoroutine(DisableMovement());
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player") && !collision.isTrigger)
+        {
+            cam.SetActive(true);
+            
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && !collision.isTrigger)
+        {
+            cam.SetActive(false);
+            startTransition = true;
+        }
+    }
+    IEnumerator DisableMovement()
+    {
+        Debug.Log("Fuck");
+        startTransition = false;
+        move.canMove = false;
+        yield return new WaitForSecondsRealtime(.05f);
+        move.Halt();
+        yield return new WaitForSecondsRealtime(.4f);
+        move.canMove = true;
+        Debug.Log("Fuck 2");
+    }
+}
