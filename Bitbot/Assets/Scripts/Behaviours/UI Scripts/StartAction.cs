fec1f89f7fafb7743a69ec931d888e0f6a67b41f
issyl <aldelleo@github.com>
2020-09-08 00:07:41 +0700

    Main Menu

---
+++ Bitbot/Assets/Scripts/Behaviours/UI Scripts/StartAction.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartAction : MonoBehaviour
+{
+
+    public Button button;
+
+
+
+    public void LoadScene(string sceneToLoad)
+    {
+        SceneManager.LoadSceneAsync(sceneToLoad);
+        Debug.Log("uwu");
+    }
+}
