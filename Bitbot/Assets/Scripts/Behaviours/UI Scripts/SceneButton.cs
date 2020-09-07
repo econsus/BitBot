24f16cb871e7ea6a38d2cec805cfe30992a4d6e2
YaBoiJager <cragmacfield@gmail.com>
2020-09-08 01:11:13 +0700

    Main menu tapi iso

---
+++ Bitbot/Assets/Scripts/Behaviours/UI Scripts/SceneButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneButton : MonoBehaviour
+{
+    public Button menuButton;
+    public void LoadScene(string sceneToLoad)
+    {
+        SceneManager.LoadSceneAsync(sceneToLoad);
+    }
+}
