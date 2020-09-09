5f1d5c7af067ed19b9ab4f99cbac87d19f10dab7
issyl <aldelleo@github.com>
2020-09-09 10:07:24 +0700

    Revert "Main Menu"
    This reverts commit 32032e657e4f1638b4bc3b08b1df097249b633eb.

--- Bitbot/Assets/Scripts/Behaviours/UI Scripts/StartAction.cs
+++ Bitbot/Assets/Scripts/Behaviours/UI Scripts/StartAction.cs
@@ -6,20 +6,12 @@ public class StartAction : MonoBehaviour
 {
 
     public Button button;
+
+
+
     public void LoadScene(string sceneToLoad)
     {
         SceneManager.LoadSceneAsync(sceneToLoad);
         Debug.Log("uwu");
-    }
-
-    public void PlayGame()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        Debug.Log("Start");
-    }
-    public void ExitButton()
-    {
-        Application.Quit();
-        Debug.Log("Exit");
     }
 }
